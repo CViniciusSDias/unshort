0b57d91dbfb02399c6257099e82fb0488b01d2a2
Vinicius Dias <carlosv775@gmail.com>
2023-12-30 17:36:13 -0300

    Project creation

---
+++ unshort.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+if ($argc < 2) {
+    echo "Usage: php unshort.php <url>\n";
+    exit(1);
+}
+
+$url = $argv[1];
+
+if (filter_var($url, FILTER_VALIDATE_URL) === false) {
+    echo "Invalid URL: $url\n";
+    exit(1);
+}
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curl, CURLOPT_HEADER, true);
+curl_setopt($curl, CURLOPT_NOBODY, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = curl_exec($curl);
+preg_match_all('/[Ll]ocation: (.+?)\r\n/', $headers, $matches);
+
+$redirects = $matches[1];
+echo "\033[1mRedirects\033[0m\n";
+foreach ($redirects as $redirect) {
+    echo "* $redirect\n";
+}
+
